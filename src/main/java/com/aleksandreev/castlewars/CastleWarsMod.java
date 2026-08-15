2576d08e298ae37e943d5368219c16091d34b833
aleksandreev2 <sashahumortele3@gmail.com>
2026-08-15 20:35:19 +0400

    fix: avoid double-registering event subscribers

--- src/main/java/com/aleksandreev/castlewars/CastleWarsMod.java
+++ src/main/java/com/aleksandreev/castlewars/CastleWarsMod.java
@@ -1,7 +1,5 @@
 package com.aleksandreev.castlewars;
 
-import com.aleksandreev.castlewars.command.WarsEvents;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -12,7 +10,6 @@ public final class CastleWarsMod {
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
     public CastleWarsMod() {
-        MinecraftForge.EVENT_BUS.register(WarsEvents.class);
         LOGGER.info("Castle Wars initialized");
     }
 }
