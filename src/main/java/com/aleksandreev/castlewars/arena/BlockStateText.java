ef3e292be592813434c072ba44da43b3bf21eef6
aleksandreev2 <sashahumortele3@gmail.com>
2026-08-15 20:39:23 +0400

    feat: add strict block-state parser helper

---
+++ src/main/java/com/aleksandreev/castlewars/arena/BlockStateText.java
@@ -0,0 +1,21 @@
+package com.aleksandreev.castlewars.arena;
+
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.minecraft.block.BlockState;
+import net.minecraft.command.arguments.BlockStateParser;
+
+final class BlockStateText {
+    private BlockStateText() {
+    }
+
+    static BlockState parseRequired(String encodedState) {
+        try {
+            BlockStateParser parser = new BlockStateParser(new StringReader(encodedState), true);
+            parser.parse(false);
+            return parser.getState();
+        } catch (CommandSyntaxException ex) {
+            throw new IllegalStateException("Invalid built-in block state: " + encodedState, ex);
+        }
+    }
+}
